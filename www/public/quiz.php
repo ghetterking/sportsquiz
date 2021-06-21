3cc1d047b8735f8d53da6cdfe1cb10456bcabf62
stylefollower <82804404+stylefollower@users.noreply.github.com>
2021-06-21 16:38:51 +0200

    commit 1

---
+++ www/public/quiz.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<!--code by webdevtrick (webdevtrick.com) -->
+<head>
+ <meta charset=UTF-8" />
+ 
+ <title>PHP QUIZ | Webdevtrick.com</title>
+ 
+ <link rel="stylesheet" type="text/css" href="style.css" />
+ <link rel="stylesheet" href="result.php">
+</head>
+ 
+<body>
+ 
+ <div id="page-wrap">
+ 
+ <h1>Skateboard Quiz</h1>
+ 
+ <form action="result.php" method="post" id="quiz">
+ 
+            <ol>
+            
+                <li>
+                
+                    <h3>How many wheels does a skateboard have?</h3>
+                    
+                    <div id="q1">
+
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                        <label for="question-1-answers-A">A) 1 </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                        <label for="question-1-answers-B">B) 2</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                        <label for="question-1-answers-C">C) 3</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+                        <label for="question-1-answers-D">D) 4</label>
+                    </div>
+                
+                    </div>
+
+                </li>
+                
+                <li>
+                
+                    <h3>Welchen Stil gibt es im Skateboarding?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-A" value="A" />
+                        <label for="question-2-answers-A">A) Street</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B" />
+                        <label for="question-2-answers-B">B) Steps</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-C" value="C" />
+                        <label for="question-2-answers-C">C) Uphill</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-D" value="D" />
+                        <label for="question-2-answers-D">D) Slalom</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Wann wurde Skateboarding in die Olympischen Spiele aufgenommen?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-A" value="A" />
+                        <label for="question-3-answers-A">A) 1933</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B" />
+                        <label for="question-3-answers-B">B) 1968</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C" />
+                        <label for="question-3-answers-C">C) 2016</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-D" value="D" />
+                        <label for="question-3-answers-D">D) 1995</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Wann hat Skateboarding seine Premiere an den Olympischen Spielen?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-A" value="A" />
+                        <label for="question-4-answers-A">A) 2025</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-B" value="B" />
+                        <label for="question-4-answers-B">B) 2021</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-C" value="C" />
+                        <label for="question-4-answers-C">C) 2030</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-D" value="D" />
+                        <label for="question-4-answers-D">D) 1995</label>
+                    </div>
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Who is a famous skateboarder?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
+                        <label for="question-5-answers-A">A) Justin Bieber</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
+                        <label for="question-5-answers-B">B) Tony Hawk</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
+                        <label for="question-5-answers-C">C) Toni Braxton</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-D" value="D" />
+                        <label for="question-5-answers-D">D) Mike Tyson</label>
+                    </div>
+                
+                </li>
+
+                <li>
+                
+                    <h3>Where was skateboarding invented?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-6-answers" id="question-6-answers-A" value="A" />
+                        <label for="question-6-answers-A">A) Switzerland</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-6-answers" id="question-6-answers-B" value="B" />
+                        <label for="question-6-answers-B">B) Indonesia</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-6-answers" id="question-6-answers-C" value="C" />
+                        <label for="question-6-answers-C">C) California</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-6-answers" id="question-6-answers-D" value="D" />
+                        <label for="question-6-answers-D">D) Portugal</label>
+                    </div>
+                
+                </li>
+
+                <li>
+                
+                    <h3>What is a famous skateboard trick?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-7-answers" id="question-7-answers-A" value="A" />
+                        <label for="question-7-answers-A">A) Backtrip</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-7-answers" id="question-7-answers-B" value="B" />
+                        <label for="question-7-answers-B">B) 360 Flip</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-7-answers" id="question-7-answers-C" value="C" />
+                        <label for="question-7-answers-C">C) BS Nailslide</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-D" value="D" />
+                        <label for="question-7-answers-D">D) FS Strip</label>
+                    </div>
+                
+                </li>
+
+                <li>
+                
+                    <h3>Aus welchem Material werden Skateboards hergestellt?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-8-answers" id="question-8-answers-A" value="A" />
+                        <label for="question-8-answers-A">A) 7 Lagen kanadisches Ahornholz</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-8-answers" id="question-8-answers-B" value="B" />
+                        <label for="question-8-answers-B">B) 10 Lagen Eichenholz</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-8-answers" id="question-8-answers-C" value="C" />
+                        <label for="question-8-answers-C">C) 5 Lagen Buchenholz</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-10-answers" id="question-8-answers-D" value="D" />
+                        <label for="question-8-answers-D">D) 9 Lagen Fichtenholz</label>
+                    </div>
+                
+                </li>
+
+                <li>
+                
+                    <h3>Which is a famous Skateboarding Magazine?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-9-answers" id="question-9-answers-A" value="A" />
+                        <label for="question-9-answers-A">A) Basher Magazine</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-9-answers" id="question-9-answers-B" value="B" />
+                        <label for="question-9-answers-B">B) Flatearth Skateboarding</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-9-answers" id="question-9-answers-C" value="C" />
+                        <label for="question-9-answers-C">C) Chinese International Skateboarding</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-9-answers" id="question-9-answers-D" value="D" />
+                        <label for="question-9-answers-D">D) Transworld Skateboarding</label>
+                    </div>
+                
+                </li>
+
+                <li>
+                
+                    <h3>Welches ist eine bekannte Skateboard-Marke?</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-10-answers" id="question-10-answers-A" value="A" />
+                        <label for="question-10-answers-A">A) Power Skateboards</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-10-answers" id="question-10-answers-B" value="B" />
+                        <label for="question-10-answers-B">B) Girl Skateboards</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-10-answers" id="question-10-answers-C" value="C" />
+                        <label for="question-10-answers-C">C) Banana Skateboards</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-10-answers" id="question-10-answers-D" value="D" />
+                        <label for="question-10-answers-D">D) Mega Skatebaords</label>
+                    </div>
+                
+                </li>
+            
+            </ol>
+            
+            <input type="submit" value="Submit" class="submitbtn" />
+ 
+ </form>
+ 
+ </div>
+ 
+ 
+</body>
+ 
+</html>
