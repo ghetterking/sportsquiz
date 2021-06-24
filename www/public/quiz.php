09a7fab321db883679e91ddee8fae823e8066a09
dina-elsharbasi <82804158+dina-elsharbasi@users.noreply.github.com>
2021-06-24 16:24:08 +0200

    my neu changes

--- www/public/quiz.php
+++ www/public/quiz.php
@@ -1,6 +1,37 @@
 <!DOCTYPE html>
 <!--code by webdevtrick (webdevtrick.com) -->
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+ <style>
+body {
+  background-color: black;
+  font-family: cursive;
+}
+
+.glow {
+  font-size: 80px;
+  color: #fff;
+  text-align: center;
+  animation: glow 1s ease-in-out infinite alternate;
+}
+
+@-webkit-keyframes glow {
+  from {
+    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
+  }
+  
+  to {
+    text-shadow: 0 0 20px #fff, 0 0 30px #ff4da6, 0 0 40px #ff4da6, 0 0 50px #ff4da6, 0 0 60px #ff4da6, 0 0 70px #ff4da6, 0 0 80px #ff4da6;
+  }
+}
+</style>
+</head>
+<body>
+
+<h1 class="glow">Sport Quiz</h1>
+     
+</body>   
  <meta charset=UTF-8" />
  
  <title>Sport QUIZ | Webdevtrick.com</title>
@@ -13,7 +44,7 @@
  
  <div id="page-wrap">
  
- <h1>Sport Quiz</h1>
+ 
  
  <form action="result.php" method="post" id="quiz">
  
@@ -350,7 +381,7 @@
  </form>
  
  </div>
- 
+ <h1 class="glow">Submit</h1>
  
 </body>
  
