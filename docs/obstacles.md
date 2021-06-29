ca16058cc98367e75b91daceb927689200948851
Caro Wima <caernwim@hotmail.com>
2021-06-29 13:36:44 +0200

    unfinished

---
+++ docs/obstacles.md
@@ -0,0 +1,4 @@
+This is for technical problems and questions
+
+Caro, 21.06.2021: I can't figure out how to set the path for the package.json to compile the scss in to the correct folder.
+    fixed: Ksenija has asked chris for help, who removed the colon from the path.
---
+++ src/php/caroQQ.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<head>
+	<meta charset=UTF-8" />
+	
+	<title>Quiz about weird shit I do</title>
+	
+	<link rel="stylesheet" type="text/css" href="testStyle.css"/>
+</head>
+
+<body>
+
+<div class="container" style="background-color: rgb(95, 158, 160); height: 1000px;">
+        <div class="row">
+          <div class="header">
+            
+          </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-sm-2">
+            </div>
+
+            <div class="col-sm-8" style="margin: 50px 0;">
+              <center>
+               <h1>Obscure sports facts throughout history</h1>
+
+                <div class="card" style="padding: 20px; text-align: center; max-width: 400px;">
+                  <h3>A horrifying story abour sentient computers developing a hatred for humanity</h3>
+                  <img class="card-img-top" src="/src/images/sport pattern.png" alt="A pattern of sports supplies">
+                  <div class="card-body">
+                   <center>
+                     <div class="col-sm-10" style="background: rgba(95, 158, 160, 50%); margin:30px 0 ;">
+                      <p>I have no mouth and I must scream</p>
+                    </div>
+                    <div class="col-sm-10" style="background: rgba(95, 158, 160, 50%); margin:30px 0 ;">
+                      <p>I have no mouth and I must scream</p>
+                    </div>
+                    <div class="col-sm-10" style="background: rgba(95, 158, 160, 50%); margin:30px 0 ;">
+                      <p>I have no mouth and I must scream</p>
+                    </div>
+                   </center>
+                    
+                  </div>
+                  <div class="next">
+                    <button style="background-color: royalblue; border: none; padding: 10px;"> Next Question</button>
+                  </div>
+                </div>
+            </div> 
+          </center>
+
+            <div class="col-sm-2">
+            </div>
+
+        </div>
+
+      </div>
+
+	<div id="page-wrap">
+
+		
+		
+		<form action="result.php" method="post" id="quiz">
+		
+            <ol>
+            
+                <li>
+                
+                    <h3>WordPress is a...</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                        <label for="question-1-answers-A">A) Software </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                        <label for="question-1-answers-B">B) Web App</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                        <label for="question-1-answers-C">C) CMS</label>
+                    </div>
+                    
+                
+                </li>
+                
+                <li>
+                
+                    <h3>SEO is Part Of...</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-A" value="A" />
+                        <label for="question-2-answers-A">A) Video Editing</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B" />
+                        <label for="question-2-answers-B">B) Graphic Designing</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-C" value="C" />
+                        <label for="question-2-answers-C">C) Web Designing</label>
+                    </div>
+                    
+                
+                
+                </li>
+                
+                <li>
+                
+                    <h3>PHP is a....</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-A" value="A" />
+                        <label for="question-3-answers-A">A) Server Side Script</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B" />
+                        <label for="question-3-answers-B">B) Programming Language</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C" />
+                        <label for="question-3-answers-C">C) Markup Language</label>
+                    </div>
+                    
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Localhost IP is..</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-A" value="A" />
+                        <label for="question-4-answers-A">A) 192.168.0.1</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-B" value="B" />
+                        <label for="question-4-answers-B">B) 127.0.0.0</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-C" value="C" />
+                        <label for="question-4-answers-C">C) 1080:80</label>
+                    </div>
+                    
+
+            
+                <li>
+                
+                    <h3>WordPress is a...</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+                        <label for="question-1-answers-A">A) Software </label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+                        <label for="question-1-answers-B">B) Web App</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+                        <label for="question-1-answers-C">C) CMS</label>
+                    </div>
+                    
+                
+                
+                </li>
+                
+                <li>
+                
+                    <h3>SEO is Part Of...</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-A" value="A" />
+                        <label for="question-2-answers-A">A) Video Editing</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-B" value="B" />
+                        <label for="question-2-answers-B">B) Graphic Designing</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-2-answers" id="question-2-answers-C" value="C" />
+                        <label for="question-2-answers-C">C) Web Designing</label>
+                    </div>
+                    
+                
+                </li>
+                
+                <li>
+                
+                    <h3>PHP is a....</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-A" value="A" />
+                        <label for="question-3-answers-A">A) Server Side Script</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-B" value="B" />
+                        <label for="question-3-answers-B">B) Programming Language</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-3-answers" id="question-3-answers-C" value="C" />
+                        <label for="question-3-answers-C">C) Markup Language</label>
+                    </div>
+                    
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Localhost IP is..</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-A" value="A" />
+                        <label for="question-4-answers-A">A) 192.168.0.1</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-B" value="B" />
+                        <label for="question-4-answers-B">B) 127.0.0.0</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-4-answers" id="question-4-answers-C" value="C" />
+                        <label for="question-4-answers-C">C) 1080:80</label>
+                    </div>
+                    
+                
+                </li>
+                
+                <li>
+                
+                    <h3>Webdevtrick Is For</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
+                        <label for="question-5-answers-A">A) Web Designer</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
+                        <label for="question-5-answers-B">B) Web Developer</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
+                        <label for="question-5-answers-C">C) Graphic Designer</label>
+                    </div>
+                    
+                
+                </li>
+            
+                
+                
+                <li>
+                
+                    <h3>Webdevtrick Is For</h3>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-A" value="A" />
+                        <label for="question-5-answers-A">A) Web Designer</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-B" value="B" />
+                        <label for="question-5-answers-B">B) Web Developer</label>
+                    </div>
+                    
+                    <div>
+                        <input type="radio" name="question-5-answers" id="question-5-answers-C" value="C" />
+                        <label for="question-5-answers-C">C) Graphic Designer</label>
+                    </div>
+                    
+                
+                </li>
+            
+            </ol>
+            
+            <input type="submit" value="Submit" class="submitbtn" />
+		
+		</form>
+	
+	</div>
+
+
+</body>
+
+</html>
